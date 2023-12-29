f9484289e924e851cfb90fbde29bf0c501e1250a
cartel <119452317+cartelfx@users.noreply.github.com>
2023-12-29 17:25:41 +0400

    Update global.telegram.client.js

--- global.telegram.client.js
+++ global.telegram.client.js
@@ -4,11 +4,13 @@ const TelegramBot = require('node-telegram-bot-api');
 const discordToken = 'DISCORD_BOT_TOKEN'; 
 const telegramToken = 'TELEGRAM_BOT_TOKEN'; 
 
-const discordClient = new Client({intents: [32767]});
+const discordClient = new Client({ intents: [32767] });
 const telegramBot = new TelegramBot(telegramToken, { polling: true });
 
 discordClient.on('message', (message) => {
   const chatId = 'TELEGRAM_CHAT_ID';
-
-  telegramBot.sendMessage(chatId, `Discord'dan gelen mesaj: ${message.content}`);
+  if (message.content.startsWith('.telegramGönder')) {
+    const messageText = message.content.slice('.telegramGönder'.length).trim();
+    telegramBot.sendMessage(chatId, `Discord'dan gelen mesaj: ${messageText}`);
+  }
 });
